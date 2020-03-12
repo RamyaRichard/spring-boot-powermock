13a6bb1d1b2949535c75faea745c3311e197e4b2
RamyaRichard <shinyrichards@gmail.com>
2020-03-12 00:03:53 -0700

    # WARNING: head commit changed in the meantime
    Change-Id: I807ba96dc080fcb5870912b9aac8d36f96f624fb

--- src/main/java/com/javatechie/pm/api/dto/OrderRequest.java
+++ src/main/java/com/javatechie/pm/api/dto/OrderRequest.java
@@ -17,5 +17,58 @@ public class OrderRequest {
 	private int price;
 	private String emailId;
 	private boolean isDiscountable;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public boolean isDiscountable() {
+		return isDiscountable;
+	}
+	public void setDiscountable(boolean isDiscountable) {
+		this.isDiscountable = isDiscountable;
+	}
+	public OrderRequest(int id, String name, int quantity, int price, String emailId, boolean isDiscountable) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+		this.emailId = emailId;
+		this.isDiscountable = isDiscountable;
+	}
+	public OrderRequest() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "OrderRequest [id=" + id + ", name=" + name + ", quantity=" + quantity + ", price=" + price
+				+ ", emailId=" + emailId + ", isDiscountable=" + isDiscountable + "]";
+	}
 
 }
