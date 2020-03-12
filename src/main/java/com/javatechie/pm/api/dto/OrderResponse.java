c7dc8c4a556cc3657add1f625826dcecd2c65df9
RamyaRichard <shinyrichards@gmail.com>
2020-03-12 00:04:52 -0700

    gg

--- src/main/java/com/javatechie/pm/api/dto/OrderResponse.java
+++ src/main/java/com/javatechie/pm/api/dto/OrderResponse.java
@@ -13,5 +13,36 @@ public class OrderResponse {
 	private OrderRequest response;
 	private String message;
 	private int statusCode;
+	public OrderRequest getResponse() {
+		return response;
+	}
+	public void setResponse(OrderRequest response) {
+		this.response = response;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public OrderResponse(OrderRequest response, String message, int statusCode) {
+		super();
+		this.response = response;
+		this.message = message;
+		this.statusCode = statusCode;
+	}
+	public OrderResponse() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "OrderResponse [response=" + response + ", message=" + message + ", statusCode=" + statusCode + "]";
+	}
 
 }
